aa7c6ab7702b8102dc2624f4eee149f0c02d4749
etigerstudio <etigerstudio@gmail.com>
2020-02-02 16:04:51 +0800

    impl: stdout consolidator.

---
+++ consolidator/stdout.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2020 E-Tiger Studio. All rights reserved.
+
+package consolidator
+
+import (
+	"errors"
+	"fmt"
+	"github.com/etigerstudio/subr"
+)
+
+type stdout struct {
+	key string
+}
+
+func (s *stdout) Consolidate(c *subr.Context) error {
+	bytes, ok := c.Buckets[s.key].([]byte)
+	if !ok {
+		return errors.New("cannot read bucket: " + s.key +" as bytes")
+	}
+	fmt.Print(string(bytes))
+	return nil
+}
+
+func NewStdout(key string) *stdout {
+	return &stdout{key: key}
+}
